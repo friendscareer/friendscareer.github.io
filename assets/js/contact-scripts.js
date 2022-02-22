1666ca72540f77cbda28a0d4be56f86f07a7366b
Eli250 <hirwaeli@outlook.com>
2022-02-22 12:05:27 +0200

    Sending Email

--- assets/js/contact-scripts.js
+++ assets/js/contact-scripts.js
@@ -4,13 +4,15 @@ const mailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const phonePattern = /^\d{10}$/;
 const error = document.querySelector("#err-msg");
 
-contactForm.addEventListener("submit", function (e) {
+contactForm.addEventListener("submit", async function (e) {
   e.preventDefault();
 
   let sender = document.forms["contact-form"]["name"].value;
   let email = document.forms["contact-form"]["email"].value;
   let phone = document.forms["contact-form"]["phone"].value;
   let message = document.forms["contact-form"]["message"].value;
+
+  let pieSpinner = document.querySelector(".lds-dual-ring");
 
   if (sender === "") {
     error.innerHTML = "Your Name is Required!";
@@ -30,10 +32,30 @@ contactForm.addEventListener("submit", function (e) {
       reply_to: email,
       from_email: email,
     };
-    emailjs
+
+    pieSpinner.style.display = "inline-block";
+    await emailjs
       .send("service_oq58qob", "template_g978jvq", messsageParams)
       .then((res) => {
-        alert("Success ", res.status);
+        pieSpinner.style.display = "none";
+        if (res.status === 200) {
+          swal("Success!", "Your message has been sent!", "success").then(
+            (value) => {
+              if (value) location.reload();
+            }
+          );
+        } else {
+          swal(
+            "Failed!",
+            "Something went wrong! Please try again!",
+            "error"
+          ).then((err) => {
+            console.log(err);
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }
 });
